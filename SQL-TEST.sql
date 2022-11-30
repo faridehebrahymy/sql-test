8da25a82492b4b664373e498add5454f8cef3051
faridehebrahymy <96016472+faridehebrahymy@users.noreply.github.com>
2022-11-30 17:24:12 +0330

    Update SQL-TEST.sql

--- SQL-TEST.sql
+++ SQL-TEST.sql
@@ -1,1 +1,42 @@
+--query A1
+SELECT SUM(Quantity*UnitPrice)
+FROM sale table;
+
+--query A2
+SELECT COUNT ( DISTINCT customer) AS "Number of customer"
+FROM sale table
+;
+
+--query A3
+SELECT product, SUM(quantity)
+FROM sale table
+GROUP BY product;
+
+--query A4
+SELECT customer, count(product),count(quantity),SUM(unit price)
+FROM sale table
+GROUP BY customer
+HAVINg SUM(unit price)>=1500;
+ 
+--query A5
+
+
+
+--query A6
+
+
+--query B 
+CREAT TABLE chart ( id   int,
+            name   varchar(20),
+            manager  varchar(20),
+            manager id   int,
+            PRIMARY KEY (id),
+            FOREIGN KEY (manager id) REFRENCE chart)
+
+
+
+
+
+
+
 
